7280a93ed76bb08791da531e7f5aa23452299ee9
mega-mac-slice <mega-mac-slice@protonmail.com>
2018-10-02 11:45:10 -0600

    Update Lattice Paths.
    - Dynamic Programming Solution.

--- src/2018_10_02_1538493733/main.py
+++ src/2018_10_02_1538493733/main.py
@@ -1,30 +1,42 @@
 #!/usr/bin/env python3
 import unittest
-from typing import List
-from itertools import permutations
 
 
-def solve(n: int) -> List[str]:
-    options = ['r', 'd']
-    items = [''.join(i) for i in permutations(options * n, 2 * n)]
-    return [i for i in set(items)]
+def solve(n: int) -> int:
+    path = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+
+    for i in range(n + 1):
+        path[i][0] = 1
+        path[0][i] = 1
+
+    for y in range(1, n + 1):
+        for x in range(1, n + 1):
+            top = path[y - 1][x]
+            left = path[y][x - 1]
+            path[y][x] = top + left
+
+    return path[-1][-1]
 
 
 class TestSolution(unittest.TestCase):
     def test_1_success(self):
         result = solve(1)
-        self.assertEqual(2, len(result))
+        self.assertEqual(2, result)
 
     def test_2_success(self):
         result = solve(2)
-        self.assertEqual(6, len(result))
+        self.assertEqual(6, result)
+
+    def test_3_success(self):
+        result = solve(3)
+        self.assertEqual(6, result)
 
     def test_10_success(self):
         result = solve(10)
-        self.assertEqual(6, len(result))
+        self.assertEqual(221542, result)
 
     def test_20_success(self):
-        result = solve(10)
+        result = solve(20)
         self.assertIsNotNone(result)
 
 
